18819e0365c0d6c1f3f5ba847ed4043994756295
ja15solis <ja15.solis@gmail.com>
2022-07-16 15:49:41 -0600

    pruebas

--- First Steps/Bucles 3.py
+++ First Steps/Bucles 3.py
@@ -1,3 +1,2 @@
 for i in range(5,50,3):
-	print(f"valor de la variable {i}") #funcion f es para unir valor de una variable con un texto
-
+	print(f"valor de la variable {i}") #funcion f es para unir valor de una variable con un text
